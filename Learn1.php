3ab0b441eb09c4793f0c0bfff430d72675361e25
MrunaliniKolapkar <37430838+MrunaliniKolapkar@users.noreply.github.com>
2018-03-20 18:55:11 +0530

    Learn page

---
+++ Learn1.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+        <link type="text/css" rel="stylesheet" href="src/css/main.css">
+        <link type="text/css" rel="stylesheet" href="src/css/learnMain.css">
+            <!-- <link type="text/css" rel="stylesheet" href="src/css/Learn-card.css"> -->
+            <!-- <link type="text/css" rel="stylesheet" href="src/css/font.css"> -->
+            <!-- <link type="text/css" rel="stylesheet" href="src/css/VideoPlayer.css"> -->
+            <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif|Lato" rel="stylesheet">
+            <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+            <script src="library/bootstrap/jquery/jquery.min.js"></script>
+            <script src="library/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="preload" as="img" href="">
+
+  </head>
+
+    <body class="bc3-clean  jtbd-stacked-page">
+
+
+    <a class="a-screen-reader-text" href="#content">Skip to content</a>
+
+  <nav class="top-nav app-android__hide" role="navigation" id="top">
+    <a class="top-nav__logo" href="/"><span>Basecamp</span></a>
+    <div class="top-nav__wrap">
+      <ul class="top-nav__list">
+        <li class="top-nav__list-item"><a class="top-nav__link" href="/how-it-works">How it works</a></li>
+        <li class="top-nav__list-item"><a class="top-nav__link customers-js" href="/customers">Real results</a></li>
+        <li class="top-nav__list-item"><a class="top-nav__link" href="pricing.php">Pricing</a></li>
+        <li class="top-nav__list-item"><a class="top-nav__link" href="Learn1.php">Learn</a></li>
+        <li class="top-nav__list-item"><a class="top-nav__link" href="/support">Support</a></li>
+        <li class="top-nav__list-item"><a class="top-nav__link" href="https://3.basecamp.com/hello">Sign in</a></li>
+        <li class="top-nav__list-item"><a class="button button--primary" href="/signup">Try it FREE</a></li>
+      </ul>
+    </div>
+  </nav>
+
+      <main class="Main_class" style="font-family: 'IBM Plex Serif', serif";>
+        <h1 class="display-2" style="font-family:'IBM Plex Serif'; font-weight:bold; ">Master the Basecamp basics</h1><br>
+      <p class="subhead display-4" >No dry, laborious manuals here. These short videos will have you up and running in no time.</p>
+      <div class="hello" style="font-size:1.8rem">Want to learn Basecamp live? <a href="/classes">Sign up for an upcoming class.</a></div>
+
+<hr style ="width:100%"></hr>
+
+<div class="row" style="font-family:'IBM Plex Serif'">
+  <div class="col-sm-4">
+     <div class="card-body">
+         <blockquote class="blockquote text-left">
+            <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>How to use the HQ, Teams and Projects to manage any part of the company</b></h2>
+               <p class="card-text" style="font-size:1.8rem;">Basecamp gives you a place to share things company-wide, within a team, or for a specific project.</p>
+               <p> <strong> More:</strong> <a href="/help/3/guides/home/company-hq">Company HQ</a>, <a href="/help/3/guides/home/creating">Teams and Projects</a>.</p>
+           </blockquote>
+              </div>
+
+          </div>
+          <div class="col-sm-8">
+
+     <div class="card-body">
+
+               <figure class="screenshot-card">
+                   <div class="video-embed">
+               <iframe src="https://www.youtube.com/embed/YYcAwalYO2E?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+           </div>
+            </figure>
+
+
+      </div>
+  </div>
+  </div>
+  <hr style ="width:100%"></hr>
+
+  <div class="row" style="font-family:'IBM Plex Serif'">
+    <div class="col-sm-4">
+       <div class="card-body">
+           <blockquote class="blockquote text-left">
+              <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>How it helps to put everything in one place instead of using separate tools.</b></h2>
+                 <p class="card-text" style="font-size:1.8rem;">Each project has a set of tools that lets you put everything in one place: messages, tasks, files, schedules and more.</p>
+                 <p> <strong> More:</strong> <a href="/help/3/guides/projects">Working in projects</a>.</p>
+             </blockquote>
+                </div>
+
+            </div>
+            <div class="col-sm-8">
+
+       <div class="card-body">
+
+                 <figure class="screenshot-card">
+                     <div class="video-embed">
+                 <iframe src="https://www.youtube.com/embed/Q9_BI-5-GXQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+             </div>
+              </figure>
+
+
+        </div>
+    </div>
+    </div>
+
+
+
+    <hr style ="width:100%"></hr>
+
+    <div class="row" style="font-family:'IBM Plex Serif'">
+      <div class="col-sm-4">
+         <div class="card-body">
+             <blockquote class="blockquote text-left">
+                <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>How and why to use discussion threads instead of chats, emails or meetings</b></h2>
+                   <p class="card-text" style="font-size:1.8rem;">Get back to past decisions and easily handle multiple issues at the same time. Examples of using the Message Board and To-Do comments to discuss work instead of chats, emails and meetings.</p>
+                 </blockquote>
+                  </div>
+
+              </div>
+              <div class="col-sm-8">
+
+         <div class="card-body">
+
+                   <figure class="screenshot-card">
+                       <div class="video-embed">
+                   <iframe src="https://www.youtube.com/embed/37NdXbkRGpE?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+               </div>
+                </figure>
+
+
+          </div>
+      </div>
+      </div>
+
+      <hr style ="width:100%"></hr>
+
+      <div class="row" style="font-family:'IBM Plex Serif'">
+        <div class="col-sm-4">
+           <div class="card-body">
+               <blockquote class="blockquote text-left">
+                  <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>How to replace routine status meetings with Automatic Check-ins</b></h2>
+                     <p class="card-text" style="font-size:1.8rem;">Status meetings waste time and are boring for everyone involved. Replace them with Automatic Check-ins.</p>
+                     <p><strong>More:</strong> <a href="/help/3/guides/projects/checkins">Automatic Check-ins</a>.</p>
+                 </blockquote>
+                    </div>
+
+                </div>
+                <div class="col-sm-8">
+
+           <div class="card-body">
+
+                     <figure class="screenshot-card">
+                         <div class="video-embed">
+                     <iframe src="https://www.youtube.com/embed/FnOjj-Oe08k?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                 </div>
+                  </figure>
+
+
+            </div>
+        </div>
+        </div>
+
+
+        <hr style ="width:100%"></hr>
+
+        <div class="row" style="font-family:'IBM Plex Serif'">
+          <div class="col-sm-4">
+             <div class="card-body">
+                 <blockquote class="blockquote text-left">
+                    <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>When and how to turn off email notifications and use the Hey! menu instead</b></h2>
+                       <p class="card-text" style="font-size:1.8rem;">In a busy account, it's better to turn off your email notifications and use the Hey! menu to see what's new. Here's how.</p>
+                      <p><strong>More:</strong> <a href="/help/3/guides/notifications/overview">How notifications work</a>.</p>
+                   </blockquote>
+                      </div>
+
+                  </div>
+                  <div class="col-sm-8">
+
+             <div class="card-body">
+
+                       <figure class="screenshot-card">
+                           <div class="video-embed">
+                       <iframe src="https://www.youtube.com/embed/HYiR8o7Axck?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                   </div>
+                    </figure>
+
+
+              </div>
+          </div>
+          </div>
+
+          <hr style ="width:100%"></hr>
+
+          <div class="row" style="font-family:'IBM Plex Serif'">
+            <div class="col-sm-4">
+               <div class="card-body">
+                   <blockquote class="blockquote text-left">
+                      <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>When to split off a new project for more privacy or more focus</b></h2>
+                         <p class="card-text" style="font-size:1.8rem;">Too many people or too much stuff in a project? Then it's time to split off a separate project. Two examples shown.</p>
+                         <p> <strong> More:</strong> <a href="/help/3/guides/home/company-hq">Company HQ</a>, <a href="/help/3/guides/home/creating">Teams and Projects</a>.</p>
+                     </blockquote>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-8">
+
+               <div class="card-body">
+
+                         <figure class="screenshot-card">
+                             <div class="video-embed">
+                         <iframe src="https://www.youtube.com/embed/Np0Us5mGzdQ?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                     </div>
+                      </figure>
+
+
+                </div>
+            </div>
+            </div>
+
+            <hr style ="width:100%"></hr>
+
+            <div class="row" style="font-family:'IBM Plex Serif'">
+              <div class="col-sm-4">
+                 <div class="card-body">
+                     <blockquote class="blockquote text-left">
+                        <h5 class="card-title" style="font-size:2.4rem;text-align:left"><b>How to stop getting notifications from a particular project or team</b></h2>
+                           <p class="card-text" style="font-size:1.8rem;">If you want to keep access to a project but don't want all the notification noise, unfollow the project. Here's how.</p>
+                           <p><strong>More:</strong> <a href="/help/3/guides/notifications/notifying">Notifying, and being notified</a>.</p>
+                       </blockquote>
+                          </div>
+
+                      </div>
+                      <div class="col-sm-8">
+
+                 <div class="card-body">
+
+                           <figure class="screenshot-card">
+                               <div class="video-embed">
+                           <iframe src="https://www.youtube.com/embed/mT7AQxZqveY?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                       </div>
+                        </figure>
+
+
+                  </div>
+              </div>
+              </div>
+
+</main>
+
+
+
+      <footer class="footer app-android__hide">
+
+                <footer class="footer app">
+                <div class="footer_content">
+
+                <div class="footer_devies">
+
+                <p> <stronmg><a href="/via"><span class="footer_devices_image"> </span>
+                Basecamp 3 is Available on all OS.
+                </div>
+
+                <nav class="footer-nav-dekstop">
+                <p>
+
+                    <span class="footer__heading"><strong>More About BaseCamp:</strong></span>
+                    <span class="footer__links">
+                      our <a href="/about">Company</a>,
+                      <a href="/Blog.php/">Blog</a>,
+                      <a href="/books">Books</a>, and
+                      <a href="/newsletter">Newsletter</a>.
+
+                    </span>
+
+                    <br>
+                    <span class="footer__heading"><strong>Keep in touch:</strong></span>
+                    <span class="footer__links">
+                      <a href="email@basecamp.com">Email us</a> or find us on
+                      <a href="https://twitter.com">Twitter</a>,
+                      <a href="https://www.facebook.com">Facebook</a>, and
+                      <a href="https://instagram.com">Instagram</a>.
+                    </span>
+                    <br>
+                    <span class="footer__heading"><strong>Fine print:</strong></span>
+                    <span class="footer__links">
+                      <a href="/about/policies">Policies &amp; Terms of Service</a>,
+                      <a href="/uptime.php">Uptime</a>,
+                    </span>
+                      </p>
+                    </nav>
+
+
+                    <nav class="footer-nav-mobile">
+
+                      <h3><strong><a href="/via">Download Basecamp 3 for iOS, Android, Mac, or PC</a>.</strong></h3>
+
+                      <h3>A bit more from Basecamp:</h3>
+
+                  <ul class="footer-nav-mobile__list">
+                    <li><a href="/about">Our company</a></li>
+                    <li><a href="/Blog.php">Blog</a></li>
+                    <li><a href="/books">Books</a></li>
+                    <li><a href="/newsletter">Newsletter</a></li>
+                      </ul>
+
+                      <h3>Keep in touch:</h3>
+
+                 <ul class="footer-nav-mobile__list">
+                    <li><a href="email@basecamp.com">Email us</a></li>
+                    <li><a href="https://twitter.com>Twitter</a></li>
+                    <li><a href="https://www.facebook.com">Facebook</a></li>
+                    <li><a href="https://instagram.com/">Instagram</a></li>
+                      </ul>
+
+                      <h3>Fine print:</h3>
+                      <ul class="footer-nav-mobile__list">
+                    <li><a href="/about/policies">Policies &amp; Terms of Service</a></li>
+                    <li><a href="/3/uptime">Uptime</a></li>
+                    <li><a href="https://status.basecamp.com/">System Status</a></li>
+                      </ul>
+
+                    </nav>
+    </body>
+
+    <script>
+      window.lazyLoadOptions = {
+          data_src: "src",
+          elements_selector: "video"
+      };
+    </script>
+    <script async src="https://3.basecamp-static.com/bcxhq/assets/vendor/lazyload-8.2.0-27ccd756d8de83ffdfa74e55988120af3529b5a9c1a825f9d9af4d3dbe5514a6.js"></script>
+
+
+</html>
