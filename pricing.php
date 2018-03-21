95ea6bad0f07a7f63a361927125ee070369d6035
BhaktiChavare <BhaktiChavare@users.noreply.github.com>
2018-03-21 11:57:25 +0530

    pricing files
    main1.css and pricing.php

---
+++ pricing.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="dns-prefetch" href="https://dash.37signals.com">
+    <link rel="dns-prefetch" href="https://www.googletagmanager.com">
+    <link rel="dns-prefetch" href="https://apis.google.com">
+    <link rel="preconnect" href="https://dash.37signals.com">
+    <link rel="preconnect" href="https://www.googletagmanager.com">
+    <link rel="preconnect" href="https://apis.google.com">
+    <link type="text/css" rel="stylesheet" href="src/css/main.css">
+    <link type="text/css" rel="stylesheet" href="src/css/main1.css">
+    <link type="text/css" rel="stylesheet" href="src/css/second.css">
+    <link href="library/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- Bootstrap core JavaScript -->
+    <script src="library/bootstrap/jquery/jquery.min.js"></script>
+    <script src="library/bootstrap/js/bootstrap.min.js"></script>
+
+  </head>
+<body>
+
+  <body class="bc3-clean  jtbd-stacked-page">
+
+<noscript>
+  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XQQKL" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+
+  <a class="a-screen-reader-text" href="#content">Skip to content</a>
+
+<nav class="top-nav app-android__hide" role="navigation" id="top">
+  <a class="top-nav__logo" href="/"><span>Basecamp</span></a>
+  <div class="top-nav__wrap">
+    <ul class="top-nav__list">
+      <li class="top-nav__list-item"><a class="top-nav__link" href="/how-it-works">How it works</a></li>
+      <li class="top-nav__list-item"><a class="top-nav__link customers-js" href="/customers">Real results</a></li>
+      <li class="top-nav__list-item"><a class="top-nav__link" href="/pricing">Pricing</a></li>
+      <li class="top-nav__list-item"><a class="top-nav__link" href="/learn">Learn</a></li>
+      <li class="top-nav__list-item"><a class="top-nav__link" href="/support">Support</a></li>
+      <li class="top-nav__list-item"><a class="top-nav__link" href="https://3.basecamp.com/hello">Sign in</a></li>
+      <li class="top-nav__list-item"><a class="button button--primary" href="/signup">Try it FREE</a></li>
+    </ul>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+
+<div style="font-family:'IBM Plex Serif'">
+<h1 style="font-size:6rem;width:70%"><b>Basecamp is $99/month,</b></h1>
+<h1 class="my-4" style="width:70%"><font size="+20"><b>all-inclusive.<b></font></h1>
+<h1 class="display-4" style="width:100%"><font size="6">Not per person, not per project, just $99/month <em>period</em>.</font></h1>
+</div>
+<br>
+<main class="myport">
+<div class="container">
+        <!-- Example row of columns -->
+        <div class="row" style="font-family:'IBM Plex Serif'">
+          <div class="col-md-4" style="border-right: 1px solid Gainsboro;height:150px">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>fixed, flat pricing</b></h2>
+            <h3><span class="highlight">We don’t charge per seat.</span> </h3><h3>The price doesn't go up as </h3><h3>you add people. </h3>
+
+          </div>
+
+          <div class="col-md-4" style="border-right: 1px solid Gainsboro;height:150px">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>all the tools teams need</b></h2>
+            <h3>Message boards, to-do lists,</h3><h3> schedules, realtime chat,</h3><h3> checkins, docs, and more. </h3>
+
+          </div>
+          <div class="col-md-4">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>world-class support</b></h2>
+            <h3>Knowledgeable, friendly, and</h3><h3> fast. Our support team is the</h3><h3> best there is.</h3>
+
+          </div>
+        </div>
+  </div>
+<hr style="width:100%"></hr>
+  <div class="container">
+        <!-- Example row of columns -->
+        <div class="row" style="font-family:'IBM Plex Serif'">
+          <div class="col-md-4" style="border-right: 1px solid Gainsboro;height:150px">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>100% free live classes</b></h2>
+            <h3>Switching software can be</h3><h3> tough.We’ll help you get</h3><h3> started on the right foot. </h3>
+
+          </div>
+
+          <div class="col-md-4" style="border-right: 1px solid Gainsboro;height:150px">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>rock-solid uptime</b></h2>
+            <h3>Don’t settle for flaky.</h3><h3>Basecamp has been up </h3><h3>99.99% since launch. </h3>
+
+          </div>
+          <div class="col-md-4">
+            <h2><img src="images/1.jpg" width="10" height="10"></h2>
+            <h2><b>security you can trust</b></h2>
+            <h3>We take security and privacy</h3><h3>seriously. Your work will be</h3><h3>safe with Basecamp.</h3>
+
+          </div>
+        </div>
+  </div>
+<br><br>
+
+  <h1 allign="center" style="position:relative">
+  <a class="button button--huge" href="/signup">Try Basecamp FREE for 30 days</a>
+</h1>
+<h1 allign="center" style="font-size:1.5rem;font-family:'Open Sans, sans-serif'">
+  <em>4,000 businesses signed up last week!</em>
+</p>
+<br><br><br>
+<hr></hr>
+<br><br><br>
+<div style="font-family:'IBM Plex Serif'">
+<p style="font-size:2rem"><font size="6">Save hundreds today and thousands as you grow.</font></p>
+<h1 style="width:100%;"><font size="5" color="Gray">Basecamp replaces 4-5 separate tools that<em> all</em> charge per person.</font></h1>
+<p><font size="5" color="Gray">Here's how  Basecamp  compares<em> vs.</em>  Slack + Asana, Dropbox & Gsuite :</font></p>
+</div>
+<div style="max-width: 100%;display: flex;justify-content: space-between;margin-bottom: 2em;">
+<div class="jumbotron" style="width: 46.5%;">
+  <div class="container" style="font-family:'IBM Plex Serif'">
+          <p style="font-size:20px"><b>Basecamp</b></p>
+          <p style="font-size:20px"><b>$99/month for your team</b></p>
+          <table allign="centered">
+            <tr>
+              <td><img src="images/1.jpg" width="10" height="10"></td>
+              <td>Messages</td>
+            </tr>
+          </table>
+          <ul style="font-size:15px;margin-left:auto">
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>Messages</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>Realtime chat</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>To-do lists</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>Schedules</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>Documents</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>File storage</b><br>
+            <img src="images/1.jpg" width="15" height="15">&nbsp;&nbsp;&nbsp;<b>Automatic check-ins</b><br>
+          </ul>
+        </div>
+</div>
+<div class="jumbotron" style="width: 46.5%;">
+  <div class="container" style="font-family:'IBM Plex Serif'">
+          <p style="font-size:20px"><b>Basecamp</b></p>
+          <p style="font-size:20px"><b>$99/month for your team</b></p>
+          <table allign="centered">
+            <tr>
+              <td><img src="images/1.jpg" width="10" height="10"></td>
+              <td>Messages</td>
+            </tr>
+          </table>
+          <ul style="font-size:15px">
+            <b>Messages</b><br>
+            <b>Realtime chat</b><br>
+            <b>To-do lists</b><br>
+            <b>Schedules</b><br>
+            <b>Documents</b><br>
+            <b>File storage</b><br>
+            <b>Automatic check-ins</b><br>
+          </ul>
+        </div>
+</div>
+</div>
+
+</main>
+
+  </body>
+
+  </html>
